8a4427e8ae8d94a76961e18da9baebc1b17f3e0f
Victor-Iroko <irokovictor7@gmail.com>
2026-07-25 02:55:44 +0100

    feat: setup Vite+ Nuxt project structure, configurations, and tests

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/hints",
+    "@nuxt/test-utils",
+    "@vueuse/nuxt",
+    "@nuxtjs/seo",
+    "@pinia/nuxt",
+  ],
+});
